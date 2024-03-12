4debe7a91d28f2c75ffd2be508d84e5dfdbc3962
Deemon13 <dmitriy.fedorenko13@gmail.com>
2024-03-12 12:11:27 +0300

    added full logic of app

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,31 @@ export function App() {
 	let [operator, setOperator] = useState('');
 	let [operand2, setOperand2] = useState('');
 
-	// let [result, setResult] = useState(null);
-
+	let [isCalculating, setIsCalculating] = useState(true);
 	let result = null;
 
 	const handleClickOnNmbBtn = event => {
+		isCalculating = setIsCalculating(true);
 		if (!operator) {
 			operand1 = setOperand1(Number(operand1 + event.target.textContent));
-			// console.dir('operand1', event.target.textContent);
 		}
 
 		if (operator) {
 			operand2 = setOperand2(Number(operand2 + event.target.textContent));
-			// console.dir('operand2', event.target.textContent);
 		}
 	};
 
 	const handleClickOnOperator = event => {
 		if (event.target.textContent === '+' || event.target.textContent === '-') {
 			operator = setOperator(event.target.textContent);
-			// console.dir(event.target.textContent);
+			isCalculating = setIsCalculating(true);
 		} else if (event.target.textContent === '=') {
+			isCalculating = setIsCalculating(false);
 			result =
 				operator === '+'
 					? Number(operand1) + Number(operand2)
 					: Number(operand1) - Number(operand2);
-
-			// console.log('result:', result);
-			setOperand1(result);
+			operand1 = setOperand1(result);
 			operator = setOperator('');
 			operand2 = setOperand2('');
 		} else {
@@ -47,42 +44,40 @@ export function App() {
 		operand1 = setOperand1('');
 		operator = setOperator('');
 		operand2 = setOperand2('');
-		// result = setResult(null);
 	};
 
 	let isResult = !result ? (
-		<span className={styles.display}>
+		<p className={`${styles.display}  ${isCalculating ? styles.red : styles.green}`}>
 			{operand1}
 			{operator}
 			{operand2}
-		</span>
+		</p>
 	) : (
-		<span className={styles.display}>{result}</span>
+		<p className={`${styles.display} ${isCalculating ? styles.red : styles.green}`}>
+			{result}
+		</p>
 	);
 
 	return (
 		<div className={styles.container}>
 			{' '}
 			{isResult}
-			{/* <span className={styles.display}>
-				{operand1}
-				{operator}
-				{operand2}
-			</span> */}
 			<div className={styles.buttons}>
 				<ul className={styles.numbers}>
-					{NUMS.filter(item => Number(item) || Number(item) === 0).map(item => {
-						return (
-							<li key={item} className={styles.item}>
-								<button
-									className={styles.btnNum}
-									onClick={handleClickOnNmbBtn}
-								>
-									{item}
-								</button>
-							</li>
-						);
-					})}
+					{NUMS.filter(item => Number(item) || Number(item) === 0)
+						.reverse()
+						.map(item => {
+							return (
+								<li key={item} className={styles.item}>
+									<button
+										className={styles.btnNum}
+										onClick={handleClickOnNmbBtn}
+									>
+										{item}
+									</button>
+								</li>
+							);
+						})}
 				</ul>
 				<ul className={styles.operators}>
 					{NUMS.filter(
@@ -90,20 +85,34 @@ export function App() {
 					).map(item => {
 						return (
 							<li key={item} className={styles.item}>
-								<button
-									className={styles.btnNum}
-									onClick={handleClickOnOperator}
-								>
-									{item}
-								</button>
+								{item === '=' ? (
+									<button
+										className={styles.btnNum}
+										onClick={handleClickOnOperator}
+										disabled={operand2 ? false : true}
+									>
+										{item}
+									</button>
+								) : (
+									<button
+										className={styles.btnNum}
+										onClick={handleClickOnOperator}
+									>
+										{item}
+									</button>
+								)}
 							</li>
 						);
 					})}
 				</ul>
 			</div>
-			<button className={styles.reset} onClick={handleReset}>
-				C
-			</button>
+			{NUMS.filter(item => item === 'C').map(item => {
+				return (
+					<button key={item} className={styles.reset} onClick={handleReset}>
+						{item}
+					</button>
+				);
+			})}
 		</div>
 	);
 }
